2bccef06092bb025163e1fc66fbb900c67874b1e
n <mac@MacBook-Pro-alkhas-b-Mac.local>
2024-01-08 02:34:40 +0100

    added email validation function

---
+++ lib/core/utils/constants/helper.dart
@@ -0,0 +1,17 @@
+class Helper{
+  /// Email validation
+  static bool validateEmail(String? value) {
+    RegExp regex = RegExp(
+        r'^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+    if (value == null || value.isEmpty) {
+      return false;
+    } else {
+      if (!regex.hasMatch(value)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+}
