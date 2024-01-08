c5876d615f9d65f10bd2e47b00b12244ba7e37cc
n <mac@MacBook-Pro-alkhas-b-Mac.local>
2024-01-08 03:31:56 +0100

    added unit test group to verify all of the validator funcions

---
+++ test/core/utils/constants/helper_test.dart
@@ -0,0 +1,58 @@
+import 'package:expatrio_tax_task/core/core.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+//flutter test test/core/utils/constants/helper_test.dart
+void main(){
+  group('Helper - Validators', () {
+    test('validateEmail', () {
+      var validation = Helper.validateEmail('');
+      expect(validation, false, reason: 'validateEmail empty should pass');
+
+      validation = Helper.validateEmail(null);
+      expect(validation, false, reason: 'validateEmail on null should pass');
+
+      validation = Helper.validateEmail('abs.com');
+      expect(validation, false, reason: 'validateEmail with wrong format should pass');
+
+      validation = Helper.validatePassword('abs2@email.com');
+      expect(validation, true, reason: 'validateEmail with correct format should pass');
+    });
+
+    test('validatePassword', () {
+      var validation = Helper.validatePassword('');
+      expect(validation, false, reason: 'validatePassword empty should pass');
+
+      validation = Helper.validatePassword(null);
+      expect(validation, false, reason: 'validatePassword on null should pass');
+
+      validation = Helper.validatePassword('1234');
+      expect(validation, false, reason: 'validatePassword with less than 8 letters should pass');
+
+      validation = Helper.validatePassword('12345678');
+      expect(validation, true, reason: 'validatePassword with 8 or more letters should pass');
+    });
+
+    test('validateNotEmpty', () {
+      String? year = '1990';
+      var validation = Helper.validateNotEmpty(year);
+      expect(validation, isNull, reason: 'validateNotEmpty should pass');
+    });
+
+    test('isEmpty', () {
+      var validation = Helper.isEmpty('0');
+      expect(validation, false, reason: 'isEmpty should pass');
+
+      validation = Helper.isEmpty('');
+      expect(validation, true, reason: 'isEmpty on String should pass');
+
+      validation = Helper.isEmpty(null);
+      expect(validation, true, reason: 'isEmpty on null should pass');
+
+      validation = Helper.isEmpty(Map());
+      expect(validation, true, reason: 'isEmpty on Map should pass');
+
+      validation = Helper.isEmpty([]);
+      expect(validation, true, reason: 'isEmpty on List should pass');
+    });
+  });
+}
